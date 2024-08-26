8ad2878703788f5b64bb7e25be5ae8a8d627607e
H-M-Nizum <hmnizum1714032@gmail.com>
2024-08-26 13:18:16 +0600

    [ADD] - HackerRank Subdomains base folder and solve Basic Select sql problem

---
+++ HackerRank/Basic Select/Weather Observation Station 12.sql
@@ -0,0 +1,23 @@
+/*
+Enter your query here.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT LIKE 'A%' AND CITY NOT LIKE 'a%' 
+  AND CITY NOT LIKE 'E%' AND CITY NOT LIKE 'e%' 
+  AND CITY NOT LIKE 'I%' AND CITY NOT LIKE 'i%' 
+  AND CITY NOT LIKE 'O%' AND CITY NOT LIKE 'o%'
+  AND CITY NOT LIKE 'U%' AND CITY NOT LIKE 'u%' 
+  
+  AND CITY NOT LIKE '%A' AND CITY NOT LIKE '%a' 
+  AND CITY NOT LIKE '%E' AND CITY NOT LIKE '%e' 
+  AND CITY NOT LIKE '%I' AND CITY NOT LIKE '%i' 
+  AND CITY NOT LIKE '%O' AND CITY NOT LIKE '%o'
+  AND CITY NOT LIKE '%U' AND CITY NOT LIKE '%u'
+ 
+  
+/*-----------------------------------------using advance --------------------------*/
+-- SELECT DISTINCT CITY
+-- FROM STATION
+-- WHERE NOT REGEXP_LIKE(CITY, '^[aeiouAEIOU]') 
+--   AND NOT REGEXP_LIKE(CITY, '[aeiouAEIOU]$');
